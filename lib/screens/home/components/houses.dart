d51719506a9570a29d15ee2150c7b09944f7d1fc
simonojok19 <simonojok19@gmail.com>
2020-11-09 20:34:47 +0300

    added the address

--- lib/screens/home/components/houses.dart
+++ lib/screens/home/components/houses.dart
@@ -1,4 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+
+import '../../../constants/constants.dart';
+import '../../../constants/constants.dart';
+import '../../../constants/constants.dart';
+import '../../../constants/constants.dart';
+import '../../../constants/constants.dart';
+import '../../../constants/constants.dart';
+import '../../../constants/constants.dart';
+import '../../../constants/constants.dart';
+import '../../../constants/constants.dart';
+import '../../../data/data.dart';
+import '../../../data/data.dart';
+import '../../../model/house.dart';
 
 class Houses extends StatefulWidget {
   @override
@@ -8,6 +22,90 @@ class Houses extends StatefulWidget {
 class _HousesState extends State<Houses> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Expanded(
+      child: ListView.builder(
+        physics: BouncingScrollPhysics(),
+        itemCount: houseList.length,
+        itemBuilder: (context, index) {
+          return _buildHouse(context, index);
+        },
+      ),
+    );
+  }
+
+  Widget _buildHouse(BuildContext context, int index) {
+    Size size =  MediaQuery.of(context).size;
+    House house = houseList[index];
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: appPadding,
+        vertical: appPadding / 2
+      ),
+      child: Container(
+        height: 250,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Stack(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image(
+                    image: AssetImage(house.imageUrl),
+                    height: 180,
+                    width: size.width,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Positioned(
+                  right: appPadding / 2,
+                  top: appPadding / 2,
+                  child: Container(
+                    child: IconButton(
+                      icon: house.isFav ? Icon(
+                        Icons.favorite_rounded,
+                        color: red,
+                      ) :  Icon(
+                        Icons.favorite_border_rounded,
+                        color: black,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          house.isFav =! house.isFav;
+                        });
+                      },
+                    ),
+                    decoration: BoxDecoration(
+                      color: white,
+                      borderRadius: BorderRadius.circular(15)
+                    ),
+                  ),
+                )
+              ],
+            ),
+            Row(
+              children: [
+                Text(
+                  '\$${house.price.toStringAsFixed(3)}',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold
+                  ),
+                ),
+                SizedBox(width: 10,),
+                Text(
+                  '\$${house.address}',
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(
+                      fontSize: 15,
+                      color: black.withOpacity(0.4)
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
