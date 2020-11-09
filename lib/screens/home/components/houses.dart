ac3f0c5c7108f93368d7d8dfacd0ac7a8db24425
simonojok19 <simonojok19@gmail.com>
2020-11-09 21:42:51 +0300

    added details screen

--- lib/screens/home/components/houses.dart
+++ lib/screens/home/components/houses.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
+import 'package:real_estate_app/screens/details/details_screen.dart';
 
 import '../../../constants/constants.dart';
 import '../../../constants/constants.dart';
@@ -36,102 +37,107 @@ class _HousesState extends State<Houses> {
   Widget _buildHouse(BuildContext context, int index) {
     Size size =  MediaQuery.of(context).size;
     House house = houseList[index];
-    return Padding(
-      padding: EdgeInsets.symmetric(
-        horizontal: appPadding,
-        vertical: appPadding / 2
-      ),
-      child: Container(
-        height: 250,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Stack(
-              children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(20),
-                  child: Image(
-                    image: AssetImage(house.imageUrl),
-                    height: 180,
-                    width: size.width,
-                    fit: BoxFit.cover,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (_) => DetailsScreen()));
+      },
+      child: Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: appPadding,
+          vertical: appPadding / 2
+        ),
+        child: Container(
+          height: 250,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Stack(
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Image(
+                      image: AssetImage(house.imageUrl),
+                      height: 180,
+                      width: size.width,
+                      fit: BoxFit.cover,
+                    ),
                   ),
-                ),
-                Positioned(
-                  right: appPadding / 2,
-                  top: appPadding / 2,
-                  child: Container(
-                    child: IconButton(
-                      icon: house.isFav ? Icon(
-                        Icons.favorite_rounded,
-                        color: red,
-                      ) :  Icon(
-                        Icons.favorite_border_rounded,
-                        color: black,
+                  Positioned(
+                    right: appPadding / 2,
+                    top: appPadding / 2,
+                    child: Container(
+                      child: IconButton(
+                        icon: house.isFav ? Icon(
+                          Icons.favorite_rounded,
+                          color: red,
+                        ) :  Icon(
+                          Icons.favorite_border_rounded,
+                          color: black,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            house.isFav =! house.isFav;
+                          });
+                        },
                       ),
-                      onPressed: () {
-                        setState(() {
-                          house.isFav =! house.isFav;
-                        });
-                      },
+                      decoration: BoxDecoration(
+                        color: white,
+                        borderRadius: BorderRadius.circular(15)
+                      ),
                     ),
-                    decoration: BoxDecoration(
-                      color: white,
-                      borderRadius: BorderRadius.circular(15)
+                  )
+                ],
+              ),
+              Row(
+                children: [
+                  Text(
+                    '\$${house.price.toStringAsFixed(3)}',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold
                     ),
                   ),
-                )
-              ],
-            ),
-            Row(
-              children: [
-                Text(
-                  '\$${house.price.toStringAsFixed(3)}',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold
+                  SizedBox(width: 10,),
+                  Expanded(
+                    child: Text(
+                      '\$${house.address}',
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                          fontSize: 15,
+                          color: black.withOpacity(0.4)
+                      ),
+                    ),
                   ),
-                ),
-                SizedBox(width: 10,),
-                Expanded(
-                  child: Text(
-                    '\$${house.address}',
-                    overflow: TextOverflow.ellipsis,
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    '${house.bedRooms} bedrooms / ',
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+                  Text(
+                    '${house.bathRooms} bathrooms / ',
                     style: TextStyle(
                         fontSize: 15,
-                        color: black.withOpacity(0.4)
+                        fontWeight: FontWeight.bold
                     ),
                   ),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  '${house.bedRooms} bedrooms / ',
-                  style: TextStyle(
-                    fontSize: 15,
-                    fontWeight: FontWeight.bold
+                  Text(
+                    '${house.sqFeet} sqFT',
+                    style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold
+                    ),
                   ),
-                ),
-                Text(
-                  '${house.bathRooms} bathrooms / ',
-                  style: TextStyle(
-                      fontSize: 15,
-                      fontWeight: FontWeight.bold
-                  ),
-                ),
-                Text(
-                  '${house.sqFeet} sqFT',
-                  style: TextStyle(
-                      fontSize: 15,
-                      fontWeight: FontWeight.bold
-                  ),
-                ),
-              ],
-            )
-          ],
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
